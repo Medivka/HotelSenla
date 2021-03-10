ef3eb77ddff7908573c367db0321e6fe0e66def8
Sacuta Vitalik <73660656+Medivka@users.noreply.github.com>
2021-03-10 21:00:00 +0300

    started create gui module JavaFX.

--- GUI/src/controllers/MainController.java
+++ GUI/src/controllers/MainController.java
@@ -1,9 +1,16 @@
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -33,8 +40,31 @@ public class MainController {
 
     @FXML
     void initialize() {
-createNewOrderField.setOnAction(actionEvent -> System.out.println("click createNewOrder") );
-historyMenuField.setOnAction(actionEvent -> System.out.println("click history menu ") );
+createNewOrderField.setOnAction(actionEvent -> openNewWindow(orderMenuPath) );
+historyMenuField.setOnAction(actionEvent -> openNewWindow(historyMenuPath) );
+serviceMenuField.setOnAction(actionEvent -> openNewWindow(serviceMenuPath));
+guestMenuField.setOnAction(actionEvent -> openNewWindow(guestMenuPath));
+orderMenuField.setOnAction(actionEvent -> openNewWindow(orderMenuPath));
+roomMenuField.setOnAction(actionEvent -> openNewWindow(roomMenuPath));
+    }
+    private String guestMenuPath="/resourses/guestMenu.fxml";
+    private String orderMenuPath="/resourses/orderMenu.fxml";
+    private String roomMenuPath="/resourses/roomMenu.fxml";
+    private  String historyMenuPath="/resourses/historyMenu.fxml";
+    private  String serviceMenuPath="/resourses/serviceMenu.fxml";
+
+    public void openNewWindow(String window){
+        FXMLLoader loader= new FXMLLoader();
+        loader.setLocation(getClass().getResource(window));
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Parent root=loader.getRoot();
+        Stage stage=new Stage();
+        stage.setScene(new Scene(root));
+        stage.show();
 
     }
 }
