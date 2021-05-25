39a48152c50edb7aa0a5b798d36cec9d11d5bcdc
Sacuta Vitalik <73660656+Medivka@users.noreply.github.com>
2021-05-25 21:43:55 +0300

    +DTO +Mapper

--- src/main/java/MainProgramm.java
+++ src/main/java/MainProgramm.java
@@ -1,4 +1,5 @@
 import com.dto.GuestDTO;
+import com.dto.OrderDTO;
 import com.mappingDTO.MappingDTO;
 import com.springConfig.SpringBeans;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,9 @@ public class MainProgramm {
 
 
         SpringBeans.init();
+        OrderDTO orderDTO=SpringBeans.mappingDTO.mapOrderToOrderDTO(SpringBeans.fasadOrder.findByID(2));
         GuestDTO guestDTO = SpringBeans.mappingDTO.mapGuestToGuestDTO(SpringBeans.fasadGuest.findById(2));
-        System.out.println(guestDTO);
+        System.out.println(orderDTO);
         System.out.println(SpringBeans.mappingDTO.mapGuestDtoTOGuest(guestDTO));
 
     }}
--- src/main/java/com/mappingDTO/MappingDTO.java
+++ src/main/java/com/mappingDTO/MappingDTO.java
@@ -1,8 +1,11 @@
 package com.mappingDTO;
 
 import com.dto.GuestDTO;
+import com.dto.OrderDTO;
 import com.dto.RoomDTO;
+import com.dto.ServiceDTO;
 import com.model.Guest;
+import com.model.Order;
 import com.model.Room;
 import org.springframework.stereotype.Service;
 
@@ -35,8 +38,9 @@ public class MappingDTO {
         guest.setAddress(guestDTO.getAddress());
         return guest;
     }
-    public RoomDTO mapRoomToRoomDTO(Room room){
-        RoomDTO roomDTO=new RoomDTO();
+
+    public RoomDTO mapRoomToRoomDTO(Room room) {
+        RoomDTO roomDTO = new RoomDTO();
         roomDTO.setId(room.getId());
         roomDTO.setRoomStatus(room.getRoomStatus());
         roomDTO.setCapacity(room.getCapacity());
@@ -45,8 +49,9 @@ public class MappingDTO {
 
         return roomDTO;
     }
-    public Room mapRoomDtoToRoom(RoomDTO roomDTO){
-        Room room=new Room();
+
+    public Room mapRoomDtoToRoom(RoomDTO roomDTO) {
+        Room room = new Room();
         room.setId(roomDTO.getId());
         room.setStars(roomDTO.getStars());
         room.setRoomStatus(roomDTO.getRoomStatus());
@@ -54,5 +59,44 @@ public class MappingDTO {
         return room;
     }
 
+    public ServiceDTO mapServiceToServiceDTO(com.model.Service service) {
+        ServiceDTO serviceDTO = new ServiceDTO();
+        serviceDTO.setId(service.getId());
+        serviceDTO.setName(service.getName());
+        serviceDTO.setPrice(service.getPrice());
+        return serviceDTO;
+    }
+
+    public com.model.Service mapServiceDtoToService(ServiceDTO serviceDTO) {
+        com.model.Service service = new com.model.Service();
+        service.setId(serviceDTO.getId());
+        service.setPrice(serviceDTO.getPrice());
+        service.setName(serviceDTO.getName());
+
+        return service;
+    }
+
+    public OrderDTO mapOrderToOrderDTO(Order order){
+        OrderDTO orderDTO=new OrderDTO();
+        orderDTO.setGuest(order.getGuest());
+        orderDTO.setId(order.getId());
+        orderDTO.setRoom(order.getRoom());
+        orderDTO.setAllAmount(order.getAllAmount());
+        orderDTO.setServices(order.getServices());
+        orderDTO.setLocalDate(order.getLocalDate());
+        orderDTO.setDaysOfStay(order.getDaysOfStay());
+        return orderDTO;
+    }
+    public Order mapOrderDtoToOrder(OrderDTO orderDTO){
+        Order order= new Order();
+        order.setGuest(orderDTO.getGuest());
+        order.setId(orderDTO.getId());
+        order.setRoom(orderDTO.getRoom());
+        order.setAllAmount(orderDTO.getAllAmount());
+        order.setServices(orderDTO.getServices());
+        order.setLocalDate(orderDTO.getLocalDate());
+        order.setDaysOfStay(orderDTO.getDaysOfStay());
+        return order;
+    }
 
 }
